df2b40bf61447612c240d03e7384fbafbeeab218
Gurzi <pgurzeda@gmail.com>
2023-03-29 00:02:30 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+import { Footer } from "./components/Footer/Footer";
+import { Navbar } from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import "./app.scss";
+
+function App() {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <Navbar />
+        <Outlet />
+        <Footer />
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/products/:id",
+          element: <Products />,
+        },
+        {
+          path: "/product/:id",
+          element: <Product />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Home() {
+  return <div>Strona główna pełnaa gówna</div>;
+}
